45782fac7848662bd29bdc665e6ce86f3e38fb5b
Yoru <swearwings@gmail.com>
2025-12-08 21:52:25 +0000

    changed ex5 to add day

--- ex5/ft_count_harvest_iterative.py
+++ ex5/ft_count_harvest_iterative.py
@@ -6,7 +6,7 @@
 #    By: jzorreta <jzorreta@student.42lisboa.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/12/08 14:17:08 by jzorreta          #+#    #+#              #
-#    Updated: 2025/12/08 14:23:22 by jzorreta         ###   ########.fr        #
+#    Updated: 2025/12/08 21:51:07 by jzorreta         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,6 +14,6 @@ def ft_count_harvest_iterative():
     n = int(input("Enter days until harvest: "))
     day = 1
     while day <= n:
-        print(day)
+        print(f"Day {day}")
         day += 1
     print("Harvest time!")
